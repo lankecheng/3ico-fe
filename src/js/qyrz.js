3dfea1037f38f596a38524982777961edb8ae38d
黄光凯 <hgk@meitu.com>
2017-06-30 16:08:50 +0800

    第二批问题修复

--- src/js/qyrz.js
+++ src/js/qyrz.js
@@ -1,8 +1,25 @@
 (function () {
     $('#js-qyrz-select').on('change', function () {
         var $this = $(this);
+        var val = $this.val();
         $('.js-qyrz-con').hide();
 
-        $('#js-qyrz-con-' + $this.val()).show();
+        $('#js-qyrz-con-' + val).show();
+
+        if (val === 'fws') {
+            $('#js-qyrz-fws-second').show();
+        } else {
+            $('#js-qyrz-fws-second').hide();
+        }
+    });
+
+    $(document).on('click', '.js-multiple-file', function (e) {
+        $(this).siblings('input[type=file]').click();
+    });
+
+    $('.js-multiple-file-remove').on('click', function (e) {
+        var $this = $(this);
+        $this.parent().remove();
+        e.stopPropagation();
     });
 })()
