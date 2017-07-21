16eadac4bdcd1cb973b139a8cee0ddc7b452bebd
黄光凯 <hgk@meitu.com>
2017-07-21 15:24:39 +0800

    update

--- src/js/common.js
+++ src/js/common.js
@@ -6,7 +6,7 @@
             Authorization: localStorage.getItem('token'),
         }
     }).then(function (res) {
-        $('#js-login').html('<span><i class="ico-user"></i>已登录</span>');
+        $('#js-login').html('<a href="/manage/index.html#/admin"><i class="ico-user"></i>个人中心</a>');
         $('#js-register').html('<span id="js-logout" class="register">退出</span>');
     });
 
